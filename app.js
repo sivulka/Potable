08c80b5a528495a12e82434c91851f62fec40e5e
Michael Zhang <failed.down@gmail.com>
2014-11-15 17:04:10 -0500

    switched express to v3

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ var app = express();
 
 app.configure(function() {
 	app.set("port", process.env.PORT || 5000);
+    app.set("views", __dirname + "/app/server/views");
+    app.set("view engine", "jade");
+    app.locals.pretty = true;
+
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({
+		secret: "lol-so-secret",
+	}));
+    app.use(express.methodOverride());
+	app.use(stylus.middleware({
+		src: __dirname + "/web",
+	}));
+
+	app.use(express.static(__dirname + "/web"));
 });
 
 app.configure("development", function() {
