d4df585fec7a3b6e4e5e3b18a6dad0a423c6853f
Michael Zhang <failed.down@gmail.com>
2014-11-16 02:43:32 -0500

    fuck this shit

--- api/api.js
+++ api/api.js
@@ -2,6 +2,7 @@ var common = require("./common");
 var http = require("http");
 var async = require("async");
 var request = require("request");
+var sleep = require("sleep").sleep;
 
 var concat = function(a, b) {
 	var result = [];
@@ -24,30 +25,71 @@ module.exports = function(app) {
 
 	app.get("/api/calculate", function(req, res) {
 		var data = {
-			lat0: 41,
-			lat1: 43,
-			long0: -72,
-			long1: -70,
-			inc: 1
+			lat0: req.param("lat0"),
+			lat1: req.param("lat1"),
+			long0: req.param("long0"),
+			long1: req.param("long1"),
+			inc: 0.5,
 		};
+		if (!(data.lat0 && data.lat1 && data.long1 && data.long2)) {
+			res.send({
+				status: 0,
+				message: "fuck you"
+			})
+		}
 		var urls = [];
-		for(var lat=data.lat0; lat<=data.lat1; lat+=data.inc) {
-			for(var long=data.long0; long<=data.long1; long+=data.inc) {
-				urls.push("https://api.tripadvisor.com/api/partner/1.0/map/" + lat + "," + long + "?key=" + common.TA_Key);
+		for(var lat=parseFloat(data.lat0); lat<=parseFloat(data.lat1); lat+=data.inc) {
+			for(var long=parseFloat(data.long0); long<=parseFloat(data.long1); long+=data.inc) {
+				var url = "https://api.tripadvisor.com/api/partner/1.0/map/" + lat + "," + long + "?distance=25&key=" + common.TA_Key;
+				urls.push(url);
+				// console.log(url);
 			}
 		}
-		console.log("requesting "+urls.length+" urls");
+		// console.log("requesting "+urls.length+" urls");
 		var results = [];
+		var IDs = [];
 		async.each(urls, function(uri, callback) {
 			request({
 				uri: uri,
 				method: "GET",
 				timeout: 3000,
 			}, function(err, res, body) {
+				sleep(1000);
+				console.log(body);
 				var obj = JSON.parse(body);
-				results = concat(results, obj.data);
-				console.log(results.length);
-				callback();
+				if (obj.data) {
+					results = concat(results, obj.data);
+					IDs.push(parseInt(obj.location_id));
+					// console.log(results.length);
+					request({
+						uri: "https://api.tripadvisor.com/api/partner/1.0/location/" + obj.location_id + "/reviews?key=" + common.TA_Key,
+						method: "GET",
+						timeout: 3000,
+					}, function(err2, res2, body2) {
+						var obj2 = JSON.parse(body2);
+						if (obj2.data) {
+							var reviews = "";
+							var delimiter = "%%#%%";
+							for(var i=0; i<obj2.data.length; i++) {
+								reviews += obj2.data[i].text + delimiter;
+							}
+							// console.log(reviews);
+							callback();
+							/*
+							common.sql.connect(common.sql_config, function(err) {
+								if (err) {
+									// fuck
+								} else {
+									var req = new common.sql.Request();
+									req.stream = true;
+									req.query("INSERT INTO records (Latitude, Longitude, Quality, Review) VALUES (" + obj.latitude + ", " + obj.longitude + ", " + obj.rating + ", ############ NOT DONE)");
+								}
+								callback();
+							});
+							*/
+						}
+					});
+				}
 			});
 			/*http.request(url, function(result) {
 				result.on("data", function(chunk) {
