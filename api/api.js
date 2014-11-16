10358202b80edf19124c5421f630faba49c6fef0
Michael Zhang <failed.down@gmail.com>
2014-11-16 03:19:20 -0500

    fetches data successfully

--- api/api.js
+++ api/api.js
@@ -2,7 +2,7 @@ var common = require("./common");
 var http = require("http");
 var async = require("async");
 var request = require("request");
-var sleep = require("sleep").sleep;
+var sleep = require("sleep").usleep;
 
 var concat = function(a, b) {
 	var result = [];
@@ -31,82 +31,81 @@ module.exports = function(app) {
 			long1: req.param("long1"),
 			inc: 0.5,
 		};
-		if (!(data.lat0 && data.lat1 && data.long1 && data.long2)) {
+		console.dir(data);
+		if (!(data.lat0 && data.lat1 && data.long0 && data.long1)) {
 			res.send({
 				status: 0,
 				message: "fuck you"
-			})
-		}
-		var urls = [];
-		for(var lat=parseFloat(data.lat0); lat<=parseFloat(data.lat1); lat+=data.inc) {
-			for(var long=parseFloat(data.long0); long<=parseFloat(data.long1); long+=data.inc) {
-				var url = "https://api.tripadvisor.com/api/partner/1.0/map/" + lat + "," + long + "?distance=25&key=" + common.TA_Key;
-				urls.push(url);
-				// console.log(url);
+			});
+			return;
+		} else {
+			var urls = [];
+			for(var lat=parseFloat(data.lat0); lat<=parseFloat(data.lat1); lat+=data.inc) {
+				for(var long=parseFloat(data.long0); long<=parseFloat(data.long1); long+=data.inc) {
+					var url = "https://api.tripadvisor.com/api/partner/1.0/map/" + lat + "," + long + "?distance=25&key=" + common.TA_Key;
+					urls.push(url);
+					// console.log(url);
+				}
 			}
-		}
-		// console.log("requesting "+urls.length+" urls");
-		var results = [];
-		var IDs = [];
-		async.each(urls, function(uri, callback) {
-			request({
-				uri: uri,
-				method: "GET",
-				timeout: 3000,
-			}, function(err, res, body) {
-				sleep(1000);
-				console.log(body);
-				var obj = JSON.parse(body);
-				if (obj.data) {
-					results = concat(results, obj.data);
-					IDs.push(parseInt(obj.location_id));
-					// console.log(results.length);
-					request({
-						uri: "https://api.tripadvisor.com/api/partner/1.0/location/" + obj.location_id + "/reviews?key=" + common.TA_Key,
-						method: "GET",
-						timeout: 3000,
-					}, function(err2, res2, body2) {
-						var obj2 = JSON.parse(body2);
-						if (obj2.data) {
-							var reviews = "";
-							var delimiter = "%%#%%";
-							for(var i=0; i<obj2.data.length; i++) {
-								reviews += obj2.data[i].text + delimiter;
+			// console.log("requesting "+urls.length+" urls");
+			var results = [];
+			var IDs = [];
+			async.each(urls, function(uri, callback) {
+				request({
+					uri: uri,
+					method: "GET",
+					timeout: 3000,
+				}, function(err, res, body) {
+					sleep(140000);
+					if (body) {
+						var obj = JSON.parse(body);
+						if (obj.data && obj.data.length != 0) {
+							for(var i=0; i<obj.data.length; i++) {
+								IDs.push(parseInt(obj.data[i].location_id));
 							}
-							// console.log(reviews);
-							callback();
-							/*
-							common.sql.connect(common.sql_config, function(err) {
-								if (err) {
-									// fuck
-								} else {
-									var req = new common.sql.Request();
-									req.stream = true;
-									req.query("INSERT INTO records (Latitude, Longitude, Quality, Review) VALUES (" + obj.latitude + ", " + obj.longitude + ", " + obj.rating + ", ############ NOT DONE)");
-								}
-								callback();
-							});
-							*/
+							results = concat(results, obj.data);
+							console.log(IDs.length);
 						}
+					}
+					callback();
+				});
+			}, function(err) {
+				if (err) {
+					console.log("shit");
+					res.send(err);
+				} else {
+					async.each(results, function(result, callback) {
+						request({
+							uri: "https://api.tripadvisor.com/api/partner/1.0/location/" + result.location_id + "/reviews?key=" + common.TA_Key,
+							method: "GET",
+							timeout: 3000,
+						}, function(err, res, body) {
+							sleep(140000);
+							if (body) {
+								var obj = JSON.parse(body);
+								if (obj) {
+									var reviews = "";
+									for(var i=0; i<obj.data.length; i++) {
+										reviews += obj.data[i].text + "%%#%%";
+									}
+									var insert = {
+										Latitude: parseFloat(result.latitude),
+										Longitude: parseFloat(result.longitude),
+										Quality: parseInt(result.rating),
+										Reviews: reviews
+									}
+									// console.dir(insert);
+									
+								}
+							}
+							callback();
+						});
+					}, function(err) {
+						console.log("DONE");
 					});
 				}
 			});
-			/*http.request(url, function(result) {
-				result.on("data", function(chunk) {
-					results.push(chunk);
-					console.log(results.length);
-					callback();
-				});
-			});*/
-		}, function(err) {
-			if (err) {
-				console.log("shit");
-				res.send(err);
-			} else {
-				console.log("DONE");
-				res.send(results);
-			}
-		});
+		}
 	});
 
 	app.get("/api/fetch", function(req, res) {
